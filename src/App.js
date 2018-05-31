65fd07d4e8c89a748f15e199ba42564dc3108055
billywoo17 <billwu_@outlook.com>
2018-05-31 14:06:36 -0700

    working user page

--- src/App.js
+++ src/App.js
@@ -2,21 +2,48 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  state = {users: []}
+  state = {receipts: []}
 
   componentDidMount() {
-    fetch('/users')
+    fetch('/receipts')
       .then(res => res.json())
-      .then(users => this.setState({ users }));
+      .then(receipts => this.setState({ receipts }));
   }
 
+  total(receiptsArr){
+    var sum = 0;
+    receiptsArr.forEach(function(value){
+      sum += value.total
+    })
+    return sum;
+  }
   render() {
     return (
       <div className="App">
-        <h1>Final Project Team Member</h1>
-        {this.state.users.map(user =>
-          <div key={user.id}>{user.username}</div>
+        <h1>Receipts</h1>
+        <table align="center">
+        <tbody>
+        <tr>
+          <th>User</th>
+          <th>Created Date</th>
+          <th>location</th>
+          <th>Total</th>
+          <th>Status</th>
+          <th>Approved By</th>
+        </tr>
+        {this.state.receipts.map(receipt =>
+          <tr key={receipt.id}>
+          <td>{receipt.user}</td>
+          <td>{receipt.created_at}</td>
+          <td>{receipt.location}</td>
+          <td>${receipt.total}</td>
+          <td>{receipt.status}</td>
+          <td>{receipt.approve_by}</td>
+          </tr>
         )}
+        </tbody>
+        </table>
+        Total: ${this.total(this.state.receipts)}
       </div>
     );
   }
