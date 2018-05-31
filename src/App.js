1234c0b83c71e32836f5ee84f30e62f320ddee8a
Stephen <stephenicwang@gmail.com>
2018-05-31 19:46:41 +0000

    Able to console log google vision result

--- src/App.js
+++ src/App.js
@@ -2,22 +2,43 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  state = {users: []}
+    constructor(props) {
+    super(props);
+    this.state = {value: ''};
 
-  componentDidMount() {
-    fetch('/users')
-      .then(res => res.json())
-      .then(users => this.setState({ users }));
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    fetch('/image', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        url: this.state.value
+      }),
+  });
+    event.preventDefault();
   }
 
   render() {
     return (
-      <div className="App">
-        <h1>Final Project Team Member</h1>
-        {this.state.users.map(user =>
-          <div key={user.id}>{user.username}</div>
-        )}
-      </div>
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          imageurl:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+      <h1></h1>
     );
   }
 }
