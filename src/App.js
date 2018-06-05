b2387c4794053802debea788a6a18d43ea8164b8
Stephen <stephenicwang@gmail.com>
2018-06-05 04:51:57 +0000

    working modal

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react';
 import './App.css';
 // import {Link} from 'react-router-dom';
 import Moment from 'react-moment';
+import ReactModal from 'react-modal';
 
 class App extends Component {
 
   constructor () {
     super();
-    this.state = {receipts: []}
+    this.state = {
+      receipts: [],
+    }
   }
 
   componentDidMount() {
@@ -24,48 +27,77 @@ class App extends Component {
     return sum;
   }
 
-  render() {
-
-    const receiptsList = this.state.receipts.map(receipt =>
-      <li className="mdc-list-item mdc-ripple-upgraded" key = {receipt.id}>
-        <i className="fas fa-check-circle mdc-list-item__graphic green"></i>
-        <span className="mdc-list-item__text" >
-          <Moment format="DD/MM/YYYY">{receipt.date}</Moment>
-            <span className="mdc-list-item__secondary-text">
-              {receipt.location} - {receipt.description}
-            </span>
-          </span>
-        <span className="mdc-list-item__meta">
-          ${parseFloat(receipt.total/100).toFixed(2)}
-        </span>
-      </li>
-    )
-
-    return (
+render() {
+   return (
       <div className ="flex-element">
         <div className= "title">
-          <h1 className = "app-name"> <i class="fas fa-receipt"></i> Your Receipts </h1>
+          <h1 className = "app-name"> <i class="fas fa-receipt"></i> Paperless</h1>
         </div>
-        <nav class="drawer mdc-drawer mdc-drawer--permanent">
-          <div class="mdc-drawer__toolbar-spacer">
+        <nav className="drawer mdc-drawer mdc-drawer--permanent">
+          <div className="mdc-drawer__toolbar-spacer">
             <h4> Projects </h4>
           </div>
-          <div class="mdc-drawer__content">
-            <nav class="mdc-list">
-              <a class="mdc-list-item"> Inbox </a>
-              <a class="mdc-list-item"> Star </a>
+          <div className="mdc-drawer__content">
+            <nav className="mdc-list">
+              <a className="mdc-list-item"> All </a>
+              <a className="mdc-list-item"> Lighthouse </a>
+              <a className="mdc-list-item"> Buisness Trip </a>
             </nav>
           </div>
         </nav>
         <div className="App">
           <ul className="mdc-list mdc-list--two-line mdc-list--avatar-list">
-            {receiptsList}
+          { this.state.receipts.map(receipt => <Receipt { ...receipt }/> )}
           </ul>
           <div className="total">Total: ${parseFloat(this.total(this.state.receipts)/100).toFixed(2)}
           </div>
         </div>
       </div>
     );
+  }
+}
+
+class Receipt extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isModalOpen: false
+    }
+    this.toggleModal = this.toggleModal.bind(this);
+  }
+
+  toggleModal() {
+    this.setState({
+      isModalOpen: !this.state.isModalOpen
+    })
+  }
+
+  render() {
+    const { id, date, location, description, total, image_url} = this.props;
+    return (
+      <li className="mdc-list-item mdc-ripple-upgraded" key = {id} onClick={ this.toggleModal }>
+        <i className="fas fa-check-circle mdc-list-item__graphic green"></i>
+        <span className="mdc-list-item__text" >
+          <Moment format="DD/MM/YYYY">{date}</Moment>
+            <span className="mdc-list-item__secondary-text">
+              {location} - {description}
+            </span>
+          </span>
+        <span className="mdc-list-item__meta">
+          ${parseFloat(total/100).toFixed(2)}
+        </span>
+         <ReactModal
+          className="modal"
+          isOpen={this.state.isModalOpen}
+          contentLabel="Minimal Modal Example"
+          >
+          <button onClick={this.toggleModal}>Close Modal</button>
+          <p>{id}</p>
+          <img src={image_url} alt="demo"/>
+        </ReactModal>
+      </li>
+    )
   }
 }
 
