f02e8f77063d37ab225190cf5489e577d5e79c29
samsamkim <hoonshik10@gmail.com>
2018-06-05 13:08:17 -0700

    login auth

--- src/Login.js
+++ src/Login.js
@@ -2,15 +2,52 @@ import React, { Component } from 'react';
 import './Login.css';
 import {Link} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter, Route, Redirect} from 'react-router-dom'
 
 
 
 class Login extends Component {
+  constructor(props){
+    super(props)
+    this.submitForm = this.submitForm.bind(this);
+
+    this.state = {
+      email: "",
+      password: "",
+    }
+  }
+
+  submitForm() {
+    fetch('http://10.30.31.122:8080/users/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email: this.state.email,
+        password: this.state.password,
+      }),
+    })
+    .then((response) => {
+      console.log(response)
+      localStorage.setItem('jwtToken', response)
+      console.log('item stored')
+    })
+  }
+
 
   render() {
+
+// const ProtectedComponent = () => {
+//  if (authFails)
+//     return <Redirect to='/users'  />
+// }
+
+
     return (
       <section className="hello">
         <div className="loginMain">
+
 
           <div className="loginHeader">
             <i className="fas fa-receipt"></i>
@@ -18,42 +55,25 @@ class Login extends Component {
           </div>
 
 
-          <input placeholder="Username" type="text" id="text-field-" className="username mdc-text-field__input"/>
-          <input placeholder="Password" type="text" id="text-field-" className="password mdc-text-field__input"/>
+          <input placeholder="Email" type="text" id="text-field-" onChange={(input)=> this.setState({username: input})} className="email mdc-text-field__input"/>
+
+          <input placeholder="Password" type="password" id="text-field-" onChange={(input)=> this.setState({password: input})} className="password mdc-text-field__input"/>
+
 
           <div className="loginButton">
-            <button type="button" className="btn btn-lg GreenSignIn">Sign In</button>
+            <button type="button" className="btn btn-lg signInButton" onClick={this.submitForm}>Sign In</button>
 
-            <button type="button" className="btn btn-outline-light btn-lg">Sign In</button>
+            <button type="button" className="btn btn-outline-light btn-lg" onClick={this.submitForm}>Sign In</button>
           </div>
-
 
         </div>
           <section className="slide-in-bottom">
-          GRASSSSSSSSSSSSSS
+
           </section>
 
     </section>
     )
   }
 }
-
-    // return <div className='Modal'>
-    //           <Logo />
-    //           <form onSubmit= { this.props.onSubmit }>
-    //             <Input type='text' name='username' placeholder='username' />
-    //             <Input type='password' name='password' placeholder='password' />
-    //             <button> Sign In</button>
-    //           </form>
-    //           <div className='social-signin'>
-    //             <button className="fb" onClick={ this.props.onClick }><i className="fa fa-facebook" aria-hidden="true"></i></button>
-    //             <button className="tw" onClick={ this.props.onClick }><i className="fa fa-twitter" aria-hidden="true"></i></button>
-    //           </div>
-    //             <a href='#'>Lost your password ?</a>
-    //        </div>
-
-
-
-
 
 export default Login;
