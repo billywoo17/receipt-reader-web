bb2efecac569ad9cbeb967de77d9d71aff3ac3fe
samsamkim <hoonshik10@gmail.com>
2018-06-05 14:26:53 -0700

    redirected to receipts

--- src/Login.js
+++ src/Login.js
@@ -10,6 +10,8 @@ class Login extends Component {
   constructor(props){
     super(props)
     this.submitForm = this.submitForm.bind(this);
+    this.email_input = this.email_input.bind(this);
+    this.password_input = this.password_input.bind(this);
 
     this.state = {
       email: "",
@@ -17,31 +19,48 @@ class Login extends Component {
     }
   }
 
+   email_input(event) {
+      this.setState({email: event.target.value});
+      console.log(event.target.value)
+    }
+   password_input(event) {
+      this.setState({password: event.target.value});
+    }
+
+
+
   submitForm() {
+    const aBody = JSON.stringify({
+      email: this.state.email,
+      password: this.state.password,
+    });
+    console.log('abody', aBody);
     fetch('http://10.30.31.122:8080/users/login', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({
-        email: this.state.email,
-        password: this.state.password,
-      }),
+      body: aBody,
     })
     .then((response) => {
-      console.log(response)
+      if(response.status === 200){
+      this.props.history.push('/users')
       localStorage.setItem('jwtToken', response)
-      console.log('item stored')
+      }else{
+        alert("Invalid Login");
+      }
+    })
+    .catch((err) =>{
+      alert("Can't connect to the server")
+
     })
   }
 
 
   render() {
 
-// const ProtectedComponent = () => {
-//  if (authFails)
-//     return <Redirect to='/users'  />
-// }
+
+
 
 
     return (
@@ -55,9 +74,9 @@ class Login extends Component {
           </div>
 
 
-          <input placeholder="Email" type="text" id="text-field-" onChange={(input)=> this.setState({username: input})} className="email mdc-text-field__input"/>
+          <input placeholder="Email" type="text" id="text-field-" onChange={this.email_input} className="email mdc-text-field__input"/>
 
-          <input placeholder="Password" type="password" id="text-field-" onChange={(input)=> this.setState({password: input})} className="password mdc-text-field__input"/>
+          <input placeholder="Password" type="password" id="text-field-" onChange={this.password_input} className="password mdc-text-field__input"/>
 
 
           <div className="loginButton">
