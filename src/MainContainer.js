61721296b4717eb6541d41907f971d5303962752
Mel Chua <mchuam@gmail.com>
2018-06-06 21:54:50 -0700

    refactored so that we have a userscreen component and did hacky shit to make admin display work.

--- src/MainContainer.js
+++ src/MainContainer.js
@@ -7,17 +7,31 @@ import {BrowserRouter, Route} from 'react-router-dom';
 
 class MainContainer extends Component {
 
-  render() {
-    return (
-      <BrowserRouter>
-        <div>
-          <Route path='/users' component={App} />
-          <Route exact path='/' component={Login} />
-        </div>
-      </BrowserRouter>
-    )
-  }
-}
+ constructor(props) {
+   super(props);
+   this.state = {
+     admin:""
+   };
+   this.isAdmin = this.isAdmin.bind(this);
+ }
+
+ isAdmin(adminStatus){
+   this.setState({admin:adminStatus});
+ }
+   render() {
+     return (
+       <BrowserRouter>
+       <div>
+         <Route
+         exact path='/'
+         render={(props) => <Login {...props} extra={this.isAdmin} />} />
+         <Route
+         exact path='/user'
+         render={(props) => <App {...props} extra={this.state.admin} />} />
+       </div>
+       </BrowserRouter>)
+   }
+ }
 
 export default MainContainer;
 
